b1aed9b85a5a35fbaafbea3fe893e165ba342f5a
Zoltan MG <zoltanmoragarcia@gmail.com>
2021-08-20 11:48:48 -0500

    [update]: se conectó ReactJs con la API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect } from 'react';
+
 
 function App() {
+  useEffect(() => {
+    fetch('http://172.17.37.229:5000/')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editar <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
